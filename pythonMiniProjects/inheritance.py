68c3841e0aded007326b9a505cae851a9dac65ea
Vicuko <victorc.fernandez@gmail.com>
2018-01-01 12:53:27 -0700

    Create Parent method for reuse with Child class

--- pythonMiniProjects/inheritance.py
+++ pythonMiniProjects/inheritance.py
@@ -4,6 +4,10 @@ class Parent():
 		self.last_name = last_name
 		self.eye_color = eye_color
 
+	def show_info(self):
+		print ("Last Name -", self.last_name)
+		print ("Eye Color -", self.eye_color)
+
 class Child(Parent):
 	def __init__(self, last_name, eye_color, number_of_toys):
 		print ("Child Constructor called")
@@ -12,7 +16,7 @@ class Child(Parent):
 
 
 father = Parent("Maverick","blue")
-print (father.last_name, father.eye_color)
+father.show_info()
 
 little_kid = Child("Maverick","green",10)
 print (little_kid.last_name, little_kid.eye_color, little_kid.number_of_toys)
