84de597070e117899e36a6b8037403881c9bb15c
Vicuko <victorc.fernandez@gmail.com>
2017-12-31 18:16:23 -0700

    Add movies and function to open trailers in browser

--- pythonMiniProjects/movieSite/entertainment_center.py
+++ pythonMiniProjects/movieSite/entertainment_center.py
@@ -5,4 +5,19 @@ toy_story = media.Movie("Toy Story",
 						"https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
 						"https://youtu.be/KYz2wyBy3kc")
 
+avatar = media.Movie("Avatar",
+					 "A marine on an alien plantet",
+					 "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+					 "https://youtu.be/5PSNL1qE6VY")
+
+matrix = media.Movie("The Matrix",
+					 "Neo believes that Morpheus, an elusive figure considered to be the most dangerous man alive, can answer his question",
+					 "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+					 "https://youtu.be/vKQi3bBA1y8")
+
 print (toy_story.storyline)
+print (avatar.storyline)
+
+avatar.show_trailer()
+print (matrix.storyline)
+matrix.show_trailer()
