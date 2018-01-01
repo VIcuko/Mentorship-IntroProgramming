25ccbae52fcea9ac3a169f8798e6c66437cea457
Vicuko <victorc.fernandez@gmail.com>
2018-01-01 12:27:29 -0700

    Create instances of Parent and Child classes

--- pythonMiniProjects/inheritance.py
+++ pythonMiniProjects/inheritance.py
@@ -7,3 +7,10 @@ class Child(Parent):
 	def __init__(self, last_name, eye_color, number_of_toys):
 		Parent.__init__(self, last_name, eye_color)
 		self.number_of_toys = number_of_toys
+
+
+father = Parent("Maverick","blue")
+print (father.last_name, father.eye_color)
+
+little_kid = Child("Maverick","green",10)
+print (little_kid.last_name, little_kid.eye_color, little_kid.number_of_toys)
