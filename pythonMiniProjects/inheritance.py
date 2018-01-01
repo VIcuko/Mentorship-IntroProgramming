5b67e9e3683e315e749a2b47c6027c1107b80b99
Vicuko <victorc.fernandez@gmail.com>
2018-01-01 12:56:03 -0700

    Ovverride Parent method show_info in chid class Child

--- pythonMiniProjects/inheritance.py
+++ pythonMiniProjects/inheritance.py
@@ -14,6 +14,12 @@ class Child(Parent):
 		Parent.__init__(self, last_name, eye_color)
 		self.number_of_toys = number_of_toys
 
+	def show_info(self):
+		print ("Last Name -", self.last_name)
+		print ("Eye Color -", self.eye_color)
+		print ("Number of Toys -", str(number_of_toys))
+
+
 
 father = Parent("Maverick","blue")
 father.show_info()
