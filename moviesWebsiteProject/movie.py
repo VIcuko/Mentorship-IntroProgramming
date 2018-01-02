70b90934173f54bbff2bef04da2409b8325117df
Vicuko <victorc.fernandez@gmail.com>
2018-01-01 18:47:34 -0700

    Add comment to init method describing its use

--- moviesWebsiteProject/movie.py
+++ moviesWebsiteProject/movie.py
@@ -2,6 +2,8 @@ import webbrowser
 
 class Movie():
 	""" This class provides a way to store a movie related information"""
+
+	# This method initializes a Movie instance with the corresponding content saved as instance variables
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
